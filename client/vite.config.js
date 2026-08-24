bd791a51c73f7e41e62ce248e38d3eff22dc60c0
paul-mccutcheon-ionos <paul.mccutcheon@ionos.com>
2026-08-24 17:29:21 +0200

    Serve frontend on port 3000 for IONOS Marketplace compatibility

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     proxy: {
       '/api': 'http://localhost:4000',
     },
+  },
+  preview: {
+    port: 3000,
   },
 })
